9572fac96c3d66cfc40886ccab7dbcc88b5272d7
Skriipt <53712892+Skriipt@users.noreply.github.com>
2026-08-24 13:09:23 +0200

    feat: add integration constants

---
+++ custom_components/eufy_p3_ble/const.py
@@ -0,0 +1,12 @@
+"""Constants for the Eufy Smart Scale P3 BLE integration."""
+
+from __future__ import annotations
+
+from typing import Final
+
+DOMAIN: Final = "eufy_p3_ble"
+MODEL_ID: Final = "eufy T9150"
+MODEL_NAME: Final = "T9150"
+DEVICE_NAME: Final = "Eufy Smart Scale P3"
+MANUFACTURER: Final = "Eufy"
+VERSION: Final = "0.1.0"
